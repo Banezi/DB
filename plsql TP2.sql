d0cacaaaa3395f329620b7e3799c5d293c3dd941
Mamadou Bane <38168905+Banezi@users.noreply.github.com>
2019-01-23 15:20:50 +0100

    TP2 Terminé

--- plsql TP2.sql
+++ plsql TP2.sql
@@ -85,7 +85,7 @@ BEGIN
 		FETCH curseur INTO v_table_name;
 		EXIT WHEN(curseur%NOTFOUND);
 		dbms_output.put_line(v_table_name);
-		v_req := 'Create table ' || v_table_name || '_old as Select * FROM ' || v_table_name;
+		v_req := 'Create OR Replace table ' || v_table_name || '_old as Select * FROM ' || v_table_name;
 		dbms_output.put_line(v_req);
 		execute immediate v_req;
 	END LOOP;
