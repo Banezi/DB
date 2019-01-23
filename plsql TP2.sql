80bc600360031aba89059eed4b91e158ee32f2d2
Banezi <mamadoubane61@yahoo.fr>
2019-01-23 11:54:35 +0100

    TP2 A-3 en cours

--- plsql TP2.sql
+++ plsql TP2.sql
@@ -22,3 +22,27 @@ insert into SalIntervalle_F2 values ('MANAGER', 2400, 3000) ;
 insert into SalIntervalle_F2 values ('PRESIDENT', 4500, 4900) ;
 insert into SalIntervalle_F2 values ('SALESMAN', 1200, 1700) ;
 
+CREATE OR REPLACE FUNCTION salok_bane(p_job SalIntervalle_F2.job%type, salaire SalIntervalle_F2.lsal%type)
+RETURN number
+IS 
+	Cursor curseur IS SELECT job FROM SalIntervalle_F2 WHERE lsal <= salaire or hsal <= salaire;
+	v_job SalIntervalle_F2.job%type;
+BEGIN
+	OPEN curseur;
+	LOOP
+		FETCH curseur INTO v_job;
+		--dbms_output.put_line('V_JOB = ' || v_job);
+		IF(v_job is null) THEN
+			dbms_output.put_line('OUPS!');
+			RAISE no_data_found;
+		END IF;
+		EXIT WHEN (curseur%NOTFOUND);
+		dbms_output.put_line('Le salaire du job ' || p_job || ' est dans l''intervale du job ' || v_job);
+	END LOOP;
+	RETURN 1;
+EXCEPTION WHEN no_data_found THEN
+	dbms_output.put_line('Le salaire du job ' || p_job || ' n''est dans aucun intervale des job');
+	RETURN 0;
+COMMIT;
+END;
+/
--- tp2plsql.sql
+++ tp2plsql.sql
@@ -1,15 +1,14 @@
-CREATE OR REPLACE FUNCTION salok_bane(p_job SalIntervalle_F2.job%type, salaire SalIntervalle_F2.lsal%type)
-RETURN number
+-- A-3
+CREATE OR REPLACE PROCEDURE raisesalary_bane(numero_dept Dept.deptno%type, dept_name Dept.dname%type, localisation Dept.loc%type)
 IS 
-	trouve BOOLEAN := false;
-	v_job SalIntervalle_F2.job%type;
+	num_dept_exist Dept.deptno%type default 0;
 BEGIN
-	SELECT job INTO v_job FROM SalIntervalle_F2 WHERE lsal <= salaire or hsal >= salaire;
-	dbms_output.put_line('Le salaire du job ' || p_job || ' est dans l''intervale du job ' || v_job);
-	RETURN 1;
+	SELECT deptno INTO num_dept_exist FROM Dept WHERE deptno = numero_dept;
+	IF(num_dept_exist != 0) THEN
+		dbms_output.put_line('Le departement ' || numero_dept || ' existe deja!');
+	END IF;
 EXCEPTION WHEN no_data_found THEN
-	dbms_output.put_line('Le salaire du job ' || p_job || ' n''est dans aucun intervale des job');
-	RETURN 0;
+	INSERT INTO Dept Values(numero_dept, dept_name, localisation);
 COMMIT;
 END;
 /
