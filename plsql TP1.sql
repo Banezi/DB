bd1e660969436c327d4f36a3e670d7523ddfdf5f
Banezi <mamadoubane61@yahoo.fr>
2019-01-23 09:50:03 +0100

    TP1 terminé

--- plsql TP1.sql
+++ plsql TP1.sql
@@ -29,3 +29,63 @@ BEGIN
 COMMIT;
 END;
 /
+
+--3
+DECLARE
+	v_ename Emp.ename%type;
+	v_empno Emp.empno%type;
+	v_sal Emp.sal%type;
+	Cursor curseur IS SELECT ename, empno, sal FROM emp ORDER BY sal DESC; -- Limit 5; ?
+BEGIN
+	OPEN curseur;
+	FOR i IN 1..5 LOOP
+		FETCH curseur INTO v_ename, v_empno, v_sal;
+		dbms_output.put_line(v_ename || ' ' || v_empno || ' ' || v_sal);
+		INSERT INTO Temp VALUES(v_sal, v_empno, v_ename);
+	END LOOP;
+COMMIT;
+END;
+/
+
+--4
+DECLARE
+	v_ename Emp.ename%type;
+	v_empno Emp.empno%type;
+	v_sal Emp.sal%type;
+	Cursor curseur IS SELECT ename, empno, sal FROM Emp WHERE nvl(sal,0)+nvl(comm,0) > 2000;
+BEGIN
+	OPEN curseur;
+	LOOP 
+		FETCH curseur INTO v_ename, v_empno, v_sal;
+		EXIT WHEN(curseur%NOTFOUND);
+		dbms_output.put_line(v_ename || ' ' || v_empno || ' ' || v_sal);
+		INSERT INTO Temp VALUES(v_sal, v_empno, v_ename);
+	END LOOP;
+COMMIT;
+END;
+/
+
+--5
+DECLARE
+	trouve BOOLEAN := false;
+	v_ename Emp.ename%type;
+	v_empno Emp.empno%type;
+	v_sal Emp.sal%type;
+	v_mgr Emp.mgr%type;
+	v_emp Emp.empno%type default 7902; 
+BEGIN
+	LOOP 
+		SELECT empno, ename, mgr, sal INTO v_empno, v_ename, v_mgr, v_sal FROM Emp WHERE empno = v_emp;
+		--dbms_output.put_line(v_empno || ' ' || v_mgr || ' ' || v_sal);
+		IF v_sal > 4000 THEN
+			trouve := true;
+			dbms_output.put_line(v_sal || ' ' || v_ename);
+			INSERT INTO Temp(num_col1, char_col) VALUES(v_sal, v_ename);
+		ELSE
+			v_emp := v_mgr;
+		END IF;
+		EXIT WHEN(trouve = true);
+	END LOOP;
+COMMIT;
+END;
+/
--- tp1plsql.sql
+++ tp1plsql.sql
@@ -1,16 +1,23 @@
+--5
 DECLARE
-	v_numCol1 Temp.num_col1%type;
-	v_numCol2 Temp.num_col2%type;
-	v_charCol Temp.char_col%type;
-	
-	
+	trouve BOOLEAN := false;
+	v_ename Emp.ename%type;
+	v_empno Emp.empno%type;
+	v_sal Emp.sal%type;
+	v_mgr Emp.mgr%type;
+	v_emp Emp.empno%type default 7902; 
 BEGIN
-	FOR i IN 1..10 LOOP
-		IF mod(i,2)=0 THEN
-			INSERT INTO Temp Values(i, i*100, i || ' est paire');
+	LOOP 
+		SELECT empno, ename, mgr, sal INTO v_empno, v_ename, v_mgr, v_sal FROM Emp WHERE empno = v_emp;
+		--dbms_output.put_line(v_empno || ' ' || v_mgr || ' ' || v_sal);
+		IF v_sal > 4000 THEN
+			trouve := true;
+			dbms_output.put_line(v_sal || ' ' || v_ename);
+			INSERT INTO Temp(num_col1, char_col) VALUES(v_sal, v_ename);
 		ELSE
-			INSERT INTO Temp Values(i, i*100, i || ' est impair');
+			v_emp := v_mgr;
 		END IF;
+		EXIT WHEN(trouve = true);
 	END LOOP;
 COMMIT;
 END;
